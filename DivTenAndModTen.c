ab6dd5155b1658cad498a05443affd6b925a8516
Harish Rane <raneharish11@gmail.com>
2023-04-30 22:08:08 +0530

    adding all files

---
+++ DivTenAndModTen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("enter the number\n");
+    scanf("%d",&n);
+
+    printf("%d",n);
+
+    printf("  %d  /10 result",n/10);
+
+    printf(" %d mod 10 res",n%10);
+
+    return 0;
+}
